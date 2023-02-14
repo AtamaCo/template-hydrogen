c28c195af96e2210bc427f66bca04dfcdb64c039
Paul Rohrbeck <paul@atama.co>
2023-02-14 10:04:49 -0800

    fix: update allowed preview domain to composer.atama.app

--- src/components/atama/preview.client.tsx
+++ src/components/atama/preview.client.tsx
@@ -5,7 +5,7 @@ import {AtamaRenderer} from '@atamaco/renderer-react';
 
 import {components, layouts} from '~/lib/atama.client';
 
-const RenderPreview = withPreview(AtamaRenderer, 'https://studio.atama.co');
+const RenderPreview = withPreview(AtamaRenderer, 'https://composer.atama.app');
 
 export function Preview() {
   return <RenderPreview layouts={layouts} components={components} />;
